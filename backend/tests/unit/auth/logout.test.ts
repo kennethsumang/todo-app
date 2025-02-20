c1d4a145f2a963c39d057605703d4afdccbdb41f
Kenneth Sumang <dev@kennethsumang.com>
2025-02-20 17:00:46 +0800

    test: boilerplate test cases for refresh token

---
+++ backend/tests/unit/auth/logout.test.ts
@@ -0,0 +1,35 @@
+import 'reflect-metadata';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import AuthService from '../../../src/services/auth.service';
+import UserRepository from '../../../src/repositories/user.repository';
+import RefreshTokenRepository from '../../../src/repositories/refreshToken.repository';
+import chaiAsPromised from 'chai-as-promised';
+import JwtUtil from '../../../src/utils/jwt.util';
+import _ from 'lodash';
+import HashUtil from '../../../src/utils/hash.util';
+import StringUtil from '../../../src/utils/string.util';
+
+chai.use(chaiAsPromised);
+
+describe('AuthService.logout', () => {
+  let userRepositoryMock: sinon.SinonStubbedInstance<UserRepository>;
+  let refreshTokenRepositoryMock: sinon.SinonStubbedInstance<RefreshTokenRepository>;
+  let jwtUtilMock: sinon.SinonStubbedInstance<JwtUtil>;
+  let hashUtilMock: sinon.SinonStubbedInstance<HashUtil>;
+  let stringUtilMock: sinon.SinonStubbedInstance<StringUtil>;
+  let authService: AuthService;
+
+  beforeEach(() => {
+    userRepositoryMock = sinon.createStubInstance(UserRepository);
+    refreshTokenRepositoryMock = sinon.createStubInstance(RefreshTokenRepository);
+    jwtUtilMock = sinon.createStubInstance(JwtUtil);
+    hashUtilMock = sinon.createStubInstance(HashUtil);
+    stringUtilMock = sinon.createStubInstance(StringUtil);
+
+    // Inject the mock into the service
+    authService = new AuthService(refreshTokenRepositoryMock, userRepositoryMock, jwtUtilMock, hashUtilMock, stringUtilMock);
+  });
+
+  
+});
