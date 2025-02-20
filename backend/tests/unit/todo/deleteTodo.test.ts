a6962ecf40ba4820e466a6db3b77a8330ff0ecb9
Kenneth Sumang <dev@kennethsumang.com>
2025-02-20 21:37:28 +0800

    test: add happy path unit test for delete todo

---
+++ backend/tests/unit/todo/deleteTodo.test.ts
@@ -0,0 +1,52 @@
+import 'reflect-metadata';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import TodoService from '../../../src/services/todo.service';
+import UserRepository from '../../../src/repositories/user.repository';
+import chaiAsPromised from 'chai-as-promised';
+import _ from 'lodash';
+import TodoRepository from '../../../src/repositories/todo.repository';
+import { addToCurrentDate, getUtcDate } from '../../../src/utils/date.util';
+import { $Enums } from '@prisma/client';
+
+chai.use(chaiAsPromised);
+
+describe('TodoService.deleteTodo', () => {
+  let userRepositoryMock: sinon.SinonStubbedInstance<UserRepository>;
+  let todoRepository: sinon.SinonStubbedInstance<TodoRepository>;
+  let todoService: TodoService;
+
+  beforeEach(() => {
+    userRepositoryMock = sinon.createStubInstance(UserRepository);
+    todoRepository = sinon.createStubInstance(TodoRepository)
+
+    // Inject the mock into the service
+    todoService = new TodoService(userRepositoryMock, todoRepository);
+  });
+
+  it('should return successful when the todo is deleted successfully', async () => {
+    const dueAt = addToCurrentDate(30);
+    const todoId = '6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177';
+    const userId = '8ba384bc-0373-462b-8196-d35af7813739';
+    const mockUser = { id: '8ba384bc-0373-462b-8196-d35af7813739', username: 'newuser', password: 'newpassword', createdAt: new Date(), updatedAt: null };
+    const mockTodo = {
+      id: "6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177",
+      userId: "8ba384bc-0373-462b-8196-d35af7813739",
+      title: "Todo 1",
+      details: "Todo 1 Details",
+      priority: $Enums.TodoPriority.LOW,
+      status: $Enums.TodoStatus.NOT_STARTED,
+      createdAt: getUtcDate(),
+      dueAt: dueAt,
+      completedAt: null,
+      updatedAt: null
+    };
+
+    userRepositoryMock.getUserById.resolves(mockUser);
+    todoRepository.getTodoById.resolves(mockTodo);
+    todoRepository.deleteTodo.resolves(mockTodo);
+
+    const response = await todoService.deleteTodo(todoId, userId);
+    expect(response).to.deep.equal({ result: true });
+  });
+});
