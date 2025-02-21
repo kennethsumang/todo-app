5772003a6ace880b4ec56cb30b708bc755fd7870
Kenneth Sumang <dev@kennethsumang.com>
2025-02-21 16:46:42 +0800

    test: finished delete todo test cases

--- backend/tests/unit/todo/deleteTodo.test.ts
+++ backend/tests/unit/todo/deleteTodo.test.ts
@@ -49,4 +49,92 @@ describe('TodoService.deleteTodo', () => {
     const response = await todoService.deleteTodo(todoId, userId);
     expect(response).to.deep.equal({ result: true });
   });
+
+  it('should throw an error when todoId is missing', async () => {
+    const dueAt = addToCurrentDate(30);
+    const todoId = undefined;
+    const userId = '8ba384bc-0373-462b-8196-d35af7813739';
+    const mockUser = { id: '8ba384bc-0373-462b-8196-d35af7813739', username: 'newuser', password: 'newpassword', createdAt: new Date(), updatedAt: null };
+    const mockTodo = {
+      id: "6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177",
+      userId: "8ba384bc-0373-462b-8196-d35af7813739",
+      title: "Todo 1",
+      details: "Todo 1 Details",
+      priority: $Enums.TodoPriority.LOW,
+      status: $Enums.TodoStatus.NOT_STARTED,
+      createdAt: getUtcDate(),
+      dueAt: dueAt,
+      completedAt: null,
+      updatedAt: null
+    };
+
+    userRepositoryMock.getUserById.resolves(mockUser);
+    todoRepository.getTodoById.resolves(mockTodo);
+    todoRepository.deleteTodo.resolves(mockTodo);
+
+    await expect(todoService.deleteTodo(todoId, userId)).to.be.rejectedWith('Missing todoId.');
+  });
+
+  it('should throw an error when userId is missing', async () => {
+    const dueAt = addToCurrentDate(30);
+    const todoId = '6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177';
+    const userId = undefined;
+    const mockUser = { id: '8ba384bc-0373-462b-8196-d35af7813739', username: 'newuser', password: 'newpassword', createdAt: new Date(), updatedAt: null };
+    const mockTodo = {
+      id: "6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177",
+      userId: "8ba384bc-0373-462b-8196-d35af7813739",
+      title: "Todo 1",
+      details: "Todo 1 Details",
+      priority: $Enums.TodoPriority.LOW,
+      status: $Enums.TodoStatus.NOT_STARTED,
+      createdAt: getUtcDate(),
+      dueAt: dueAt,
+      completedAt: null,
+      updatedAt: null
+    };
+
+    userRepositoryMock.getUserById.resolves(mockUser);
+    todoRepository.getTodoById.resolves(mockTodo);
+    todoRepository.deleteTodo.resolves(mockTodo);
+
+    await expect(todoService.deleteTodo(todoId, userId)).to.be.rejectedWith('Missing userId.');
+  });
+
+  it('should throw an error when todo with the supplied todoId does not exist in DB', async () => {
+    const todoId = '6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177';
+    const userId = '8ba384bc-0373-462b-8196-d35af7813739';
+    const mockUser = { id: '8ba384bc-0373-462b-8196-d35af7813739', username: 'newuser', password: 'newpassword', createdAt: new Date(), updatedAt: null };
+    const mockTodo = null;
+
+    userRepositoryMock.getUserById.resolves(mockUser);
+    todoRepository.getTodoById.resolves(mockTodo);
+    todoRepository.deleteTodo.resolves(undefined);
+
+    await expect(todoService.deleteTodo(todoId, userId)).to.be.rejectedWith('Todo not found.');
+  });
+
+  it('should throw an exception when user with the supplied userId does not exist in DB', async () => {
+    const dueAt = addToCurrentDate(30);
+    const todoId = '6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177';
+    const userId = '8ba384bc-0373-462b-8196-d35af7813739';
+    const mockUser = null;
+    const mockTodo = {
+      id: "6f83a4e6-bd31-4fa1-a15b-66ffcb5ad177",
+      userId: "8ba384bc-0373-462b-8196-d35af7813739",
+      title: "Todo 1",
+      details: "Todo 1 Details",
+      priority: $Enums.TodoPriority.LOW,
+      status: $Enums.TodoStatus.NOT_STARTED,
+      createdAt: getUtcDate(),
+      dueAt: dueAt,
+      completedAt: null,
+      updatedAt: null
+    };
+
+    userRepositoryMock.getUserById.resolves(mockUser);
+    todoRepository.getTodoById.resolves(mockTodo);
+    todoRepository.deleteTodo.resolves(mockTodo);
+
+    await expect(todoService.deleteTodo(todoId, userId)).to.be.rejectedWith('User not found.');
+  });
 });
