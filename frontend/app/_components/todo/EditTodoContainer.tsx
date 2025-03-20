80baea03b89e9573d0b0cfe6b6dc9d9f486b4bb3
Kenneth Sumang <dev@kennethsumang.com>
2025-03-20 10:23:07 +0800

    feat: remove mock data implementation

--- frontend/app/_components/todo/EditTodoContainer.tsx
+++ frontend/app/_components/todo/EditTodoContainer.tsx
@@ -3,27 +3,29 @@
 import { Paper } from "@mui/material";
 import React from "react";
 import TodoForm from "./TodoFormContainer";
-import { TodoItem } from "@/app/_types/todo";
+import { useQuery } from "@tanstack/react-query";
+import requestSpecificTodo from "@/app/_requests/todo/fetch-specific-todo.request";
+import LoadingPage from "../misc/LoadingPage";
 
 interface Props {
   todoId: string;
 }
 
 const EditTodoContainer: React.FC<Props> = ({ todoId }) => {
-  const data: { todo: TodoItem } = {
-    todo: {
-      id: todoId,
-      userId: 'test',
-      title: 'Prepare Materials for Sprint Review',
-      details: 'Prepare documentation and slides for the finished stories.',
-      status: 1,
-      priority: 2,
-      createdAt: "2025-03-04T14:40:33.286Z",
-      dueAt: "2026-02-19T12:43:16.994Z",
-      completedAt: null,
-      updatedAt: null,
-    }
-  };
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todo", todoId],
+    queryFn: () => requestSpecificTodo(todoId),
+  });
+
+  if (isLoading) {
+    return <LoadingPage />
+  }
+
+  if (error || !data) {
+    console.error(error);
+    return <></>;
+  }
+
   return (
     <Paper square={false} className="flex flex-col gap-8 p-3 h-full">
       <TodoForm initialData={data.todo} />
